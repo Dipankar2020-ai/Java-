cf092c34091d81c95b9f256bbb5cee923f25831c
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-07-30 00:45:01 -0700

    Add files via upload

---
+++ Introduction/Name.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+class Name
+{
+    public static void main(String args[])
+     {
+        Scanner s=new Scanner(System.in);
+        String yourname;
+        System.out.println("May I know your name");
+        yourname=s.nextLine();
+        System.out.println("My name is "+yourname);
+
+      }
+}
