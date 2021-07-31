a423b6520db00a76098dd4f2dd0842ca21a2c423
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-07-30 23:37:23 -0700

    Create Data size & range.java

---
+++ Data Types-variables and literals/Data size & range.java
@@ -0,0 +1,18 @@
+class Datasizerange
+{
+	public static void main(String args[])
+	{  
+		System.out.println("For integer=>");
+		System.out.println("Integer Max: "+Integer.MIN_VALUE);
+		System.out.println("Integer Min: "+Integer.MAX_VALUE);
+		System.out.println("INT_BYTES :"+Integer.SIZE);
+		System.out.println("For byte=>");
+		System.out.println("Byte Max: "+Byte.MIN_VALUE);
+		System.out.println("Byte Min: "+Byte.MAX_VALUE);
+		System.out.println("Byte_BYTES :"+Byte.SIZE);
+		System.out.println("For float=>");
+		System.out.println("Float Max: "+Float.MIN_VALUE);
+		System.out.println("Float Min: "+Float.MAX_VALUE);
+		System.out.println("Float_BYTES :"+Float.SIZE);
+	}
+}
