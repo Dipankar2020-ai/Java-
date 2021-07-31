b27abe5a2aebc665765b68751c86903af9b4e393
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-07-31 03:18:16 -0700

    Add files via upload

---
+++ Data Types-variables and literals/Literal.java
@@ -0,0 +1,22 @@
+class Literal
+{
+	public static void main(String args[])
+	{
+		byte b1=10;
+		byte b2=0b1010;
+		byte b3=012;
+		byte b4=0XA;
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+		System.out.println(b4);
+		float f1=1.24f;
+		double d1=55.77;
+		long l1=99999L;
+		System.out.println(f1);
+		System.out.println(d1);
+		System.out.println(l1);
+
+
+	}
+}
