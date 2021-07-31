1897d6e7e67aa4c9b4a10fd3db43ce37a038e7ce
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-07-31 04:16:37 -0700

    Create Unicode.java

---
+++ Data Types-variables and literals/Unicode.java
@@ -0,0 +1,10 @@
+public class Unicode{
+
+     public static void main(String []args){
+        
+        for(char c=0x0980;c<=0x09F0;c++)
+        {
+            System.out.print(c+" ");
+        }
+     }
+}
