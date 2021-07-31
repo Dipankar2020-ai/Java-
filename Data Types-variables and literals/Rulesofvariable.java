69dc588a6d8aa8f5515a55cd0e46b829a4d86b29
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-07-31 02:50:03 -0700

    Add files via upload

---
+++ Data Types-variables and literals/Rulesofvariable.java
@@ -0,0 +1,13 @@
+class Rulesofvariable
+{
+	public static void main(String args[])
+	{
+		int _x=10;
+		int $x=11;
+		int String=12; //variable name as string class name
+		System.out.println(String);
+		int rollNumber=16;//camel cases
+		System.out.println(_x);
+		System.out.println($x);
+	}
+}
