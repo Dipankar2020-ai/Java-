88be40f204b7250f3d49bf1e5a55b5178c37c848
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-07-31 00:09:10 -0700

    Add files via upload

---
+++ Data Types-variables and literals/Variables.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+class Variables
+{
+	public static void main(String args[])
+	{
+		byte b=12;
+		short s=15;
+		float f=45.6f;
+		char c='D';
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(f);
+		System.out.println(c);
+
+	}
+}
