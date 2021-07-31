c8adebe89cd65a25dd89d42778ba387d2ee065b8
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-07-31 03:48:44 -0700

    Add files via upload

---
+++ Data Types-variables and literals/DatatypeDetails.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.awt.*;
+import javax.swing.*;
+
+class DatatypeDetails
+{
+	public static void main(String args[])
+	{
+		int x=5;
+		System.out.println(Integer.toBinaryString(x));
+		int _x=-5;
+        System.out.println(Integer.toBinaryString(_x));
+	}
+}
