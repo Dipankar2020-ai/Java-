2c2b98bd997e588566911fe4c525e37b36360e04
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-08-01 00:05:04 -0700

    Create Arithmetic.java

---
+++ Operator and expression/Arithmetic.java
@@ -0,0 +1,32 @@
+package arithmetic;
+
+
+public class Arithmetic {
+
+ 
+    public static void main(String[] args) {
+        // modulo operator
+        int a=9;
+        int b=2;
+        int c=a/b;
+        int d=a%b;
+        System.out.println(c);
+        System.out.println(d);
+        //coersion(type conversion)
+        float c1=(float)a/b;
+        System.out.println(c1);
+        //character to integer
+        char g=40;
+        int h=30;
+        int i=g-h;
+         System.out.println(i);
+         //modulo operator on float
+         float f1=14.3f;
+         float b1=3.1f;
+         float r=f1%b1;
+         System.out.println(r);
+         
+        
+    }
+    
+}
