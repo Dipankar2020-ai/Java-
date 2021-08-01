6f73949943ba4c6e31ceb1261fc216fefefdd54f
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-08-01 02:53:49 -0700

    Create Areaoftriangle.java

---
+++ Operator and expression/Areaoftriangle.java
@@ -0,0 +1,21 @@
+import java.util.*;
+/**
+ *
+ * @author DIPANKAR
+ */
+class Areaoftriangle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int b=s.nextInt();
+        int h=s.nextInt();
+        int area=(b*h)/2;
+        System.out.println(area);
+        
+        
+    }
+    
+}
