01d4db4fcf63a53e13bd15b5a87be6513c6d816c
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-08-02 03:47:27 -0700

    Create Printclass.java

---
+++ String class and printing/Printclass.java
@@ -0,0 +1,11 @@
+class Printclass
+{
+	public static void main(String args[])
+	{
+		int x=10,y=20;
+		System.out.println("Sum is "+x+y);
+		System.out.println(x+y+" is sum");
+		System.out.println("sum of"+x+" and "+y+" is "+(x+y));
+
+	}
+}
