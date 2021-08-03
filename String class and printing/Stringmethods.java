fd2b0d4af757217087c4ff154b2cf7886f04ae7a
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-08-02 23:50:07 -0700

    Add files via upload

---
+++ String class and printing/Stringmethods.java
@@ -0,0 +1,27 @@
+
+
+
+
+public class Stringmethods {
+
+    
+    public static void main(String[] args) {
+        String str="WELCOME";
+        String str1=str.toLowerCase();
+        int size=str.length();
+        String str2="     Dipankar    ";
+        String str3=str2.trim();
+        String str4=str.substring(3);
+        String str5=str.substring(3, 6);
+        String str6=str.replace('e', 'k');
+        System.out.println(str1);
+        System.out.println(size);
+        System.out.println(str3);
+        System.out.println(str4);
+        System.out.println(str5);
+        System.out.println(str6);
+        
+       
+    }
+    
+}
