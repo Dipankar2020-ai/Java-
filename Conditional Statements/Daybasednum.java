e07397e29b6b393a0c96bb62277cc0a826f6425d
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-08-04 08:27:41 -0700

    Create Daybasednum.java

---
+++ Conditional Statements/Daybasednum.java
@@ -0,0 +1,47 @@
+ */
+package daybasednum;
+import java.util.*;
+/**
+ *
+ * @author DIPANKAR
+ */
+public class Daybasednum {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int day=sc.nextInt();
+        if(day==1)
+        {
+            System.out.println("Monday");
+        }
+        if(day==2)
+        {
+            System.out.println("Tuesday");
+        }
+        if(day==3)
+        {
+            System.out.println("Wednesday");
+        }
+        if(day==4)
+        {
+            System.out.println("Thursday");
+        }
+        if(day==5)
+        {
+            System.out.println("Friday");
+        }
+        if(day==6)
+        {
+            System.out.println("Saturday");
+        }
+         if(day==7)
+        {
+            System.out.println("Sunday");
+        }
+        
+    }
+    
+}
