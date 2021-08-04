f901b9f6aa127ea3f71461d4adc99f7d0dcbe85c
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-08-04 03:11:20 -0700

    Create positive or negative.java

---
+++ Conditional Statements/positive or negative.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package relationaloperator;
+import java.util.*;
+
+/**
+ *
+ * @author DIPANKAR
+ */
+public class Relationaloperator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>=0)
+        {
+            System.out.println("The number is positive");
+        }
+        else
+        {
+            System.out.println("The number is negative");
+        }
+    }
+    
+}
