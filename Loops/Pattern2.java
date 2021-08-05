698e873cc66bc84be6c7b4a5963126d24ee67ed1
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-08-05 04:37:00 -0700

    Create Pattern2.java

---
+++ Loops/Pattern2.java
@@ -0,0 +1,27 @@
+package pattern1;
+import java.util.*;
+/**
+ *
+ * @author DIPANKAR
+ */
+public class Pattern1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+              System.out.print(i+" ");
+              
+            }
+            System.out.println();
+        }
+    }
+    
+}
