0d2bfe7ae148b3edf247b1f226c1718ea4e31b47
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-08-05 04:53:50 -0700

    Create  Pattern5.java

---
+++ Loops/Pattern5.java
@@ -0,0 +1,25 @@
+package pattern5;
+import java.util.*;
+/**
+ *
+ * @author DIPANKAR
+ */
+public class Pattern5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
