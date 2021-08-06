03c21de18c51215e05eac91eb60b9ada6cf91c2b
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-08-06 03:56:13 -0700

    Create Second maxium element in array.java

---
+++ Arrays/Second maxium element in array.java
@@ -0,0 +1,44 @@
+package practisngarray;
+import java.util.*;
+/**
+ *
+ * @author DIPANKAR
+ */
+public class PractisngArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int A[]=new int[size];
+       
+        for(int i=0;i<A.length;i++)
+        {
+             A[i]=sc.nextInt();
+        }
+      
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<A.length;i++)
+        {
+            if(A[i]>max)
+            {
+                max=A[i];
+            }
+        }
+        System.out.println("The maxium element of an array "+max);
+        int secondmax=Integer.MIN_VALUE;
+        for(int i=0;i<A.length;i++)
+        {
+            if(A[i]>secondmax && A[i]<max)
+            {
+                secondmax=A[i];
+            }
+        }
+      System.out.println("The Second maxium element of an array "+secondmax);
+        
+    }
+    
+}
