58e4e42c9bedc3a40b2e01e56bb6df6ab5878d6b
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-08-15 12:09:21 +0530

    Create Test2.java

---
+++ Packages/Test2.java
@@ -0,0 +1,12 @@
+import java.lang.Runnable;
+class Test2
+{
+       public static void main(String args[])
+           {
+              java.lang.String str;
+              str=new java.lang.String("Hello");
+              System.out.println(str);
+
+            }
+     
+}
