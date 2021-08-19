2d65d428cdf45d56c37a5f3f7b44f33989eeebb9
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-08-19 10:54:57 +0530

    Create synchronized.java

---
+++ MultiThreading/synchronized.java
@@ -0,0 +1,58 @@
+class My
+{
+    
+    public void display(String str)
+    {
+    	synchronized(this)
+    	{
+        for(int i=0;i<str.length();i++)
+        {
+             System.out.println(str.charAt(i));
+        }
+        }
+       
+    }
+}
+class Mythread1 extends Thread
+{
+    My d;
+    Mythread1(My dat)
+    {
+        d=dat;
+    }
+    @Override
+    public void run()
+    {
+        d.display("Hello world");
+    }
+}
+class Mythread2 extends Thread
+{
+    My d;
+    Mythread2(My data)
+    {
+        d=data;
+    }
+    @Override
+    public void run()
+    {
+        d.display("Wlocome");
+    }
+}
+
+
+public class TestJava {
+
+    
+    public static void main(String[] args) {
+        My m1=new My();
+        Mythread1 t1=new Mythread1(m1);
+        Mythread2 t2=new Mythread2(m1);
+        t1.start();
+        t2.start();
+        
+        
+        
+    }
+    
+}
