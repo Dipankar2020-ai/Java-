07f1cae7cef76e215b72093f92c8cdb67ac5de71
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-08-20 22:00:51 +0530

    Add files via upload

---
+++ Java.lang.Package/Demomath.java
@@ -0,0 +1,24 @@
+
+public class Demomath {
+
+    
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-123));
+        System.out.println(StrictMath.abs(-123));
+        System.out.println(Math.cbrt(27));
+        System.out.println(Math.getExponent(12.3456));
+        System.out.println(Math.floorDiv(50,9));
+        System.out.println(Math.exp(1));
+        System.out.println(Math.log10(100));
+        System.out.println(Math.max(100,50));
+        System.out.println(Math.min(20,90));
+        System.out.println(Math.abs(45*Math.PI/180));
+        System.out.println(Math.toRadians(90));
+        System.out.println(Math.atan(1));
+        System.out.println(Math.random());
+        System.out.println(Math.pow(3,3));
+        System.out.println(Math.multiplyExact(100,200));
+        System.out.println(Math.nextAfter(12.5,13)); 
+    }
+    
+}
